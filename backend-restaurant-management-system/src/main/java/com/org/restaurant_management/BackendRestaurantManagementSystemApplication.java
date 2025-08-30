f3c9909b1d015f1de8dd023198dd40599ada5903
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-31 04:31:24 +0600

    project init

---
+++ backend-restaurant-management-system/src/main/java/com/org/restaurant_management/BackendRestaurantManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.org.restaurant_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRestaurantManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRestaurantManagementSystemApplication.class, args);
+        System.out.println("The project is running");
+	}
+
+}
---
+++ backend-restaurant-management-system/src/test/java/com/org/restaurant_management/BackendRestaurantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.restaurant_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRestaurantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
