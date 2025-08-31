68c278fb95e85cfefec5b64c9831a14811119501
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-31 22:12:38 +0600

    login page view but not data insert

--- frontend-restaurant-mgt-syst/src/app/app.routes.ts
+++ frontend-restaurant-mgt-syst/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { SignupComponent } from './auth-components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'signup', component: SignupComponent}
+    ,{path: '', redirectTo: 'signup', pathMatch: 'full'}
+    ,{path: '**', redirectTo: 'signup'}
+];
+
+export const appRouting = provideRouter(routes);
---
+++ frontend-restaurant-mgt-syst/src/app/auth-components/signup/signup.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth-services/auth-service/auth.service';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+@Component({
+  selector: 'app-signup',
+
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+  ],
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup<{
+    username: FormControl<string>;
+    email: FormControl<string>;
+    password: FormControl<string>;
+    confirmPassword: FormControl<string>;
+  }>;
+
+  loading = false;
+
+  constructor(private service: AuthService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group(
+      {
+        username: this.fb.control('', {
+          validators: [Validators.required, Validators.minLength(3)],
+          nonNullable: true,
+        }),
+        email: this.fb.control('', {
+          validators: [Validators.required, Validators.email],
+          nonNullable: true,
+        }),
+        password: this.fb.control('', {
+          validators: [Validators.required, Validators.minLength(5)],
+          nonNullable: true,
+        }),
+        confirmPassword: this.fb.control('', {
+          validators: [Validators.required],
+          nonNullable: true,
+        }),
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+
+  // custom validator
+  passwordMatchValidator: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  };
+
+  register(): void {
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+    this.loading = true;
+    this.service.signup(this.signupForm.getRawValue()).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      },
+    });
+  }
+}
