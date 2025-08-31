5694e915732dc9edda76d1d4734543ef89e5b1d8
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-31 16:59:48 +0600

    security config working p->db

--- backend-restaurant-management-system/src/main/java/com/org/restaurant_management/configarations/WebSecurityConfig.java
+++ backend-restaurant-management-system/src/main/java/com/org/restaurant_management/configarations/WebSecurityConfig.java
@@ -1,14 +1,35 @@
 package com.org.restaurant_management.configarations;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/auth/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
+
+                .sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 }
--- backend-restaurant-management-system/src/main/java/com/org/restaurant_management/controllers/AuthController.java
+++ backend-restaurant-management-system/src/main/java/com/org/restaurant_management/controllers/AuthController.java
@@ -4,7 +4,6 @@ import com.org.restaurant_management.dtos.SignupRequest;
 import com.org.restaurant_management.dtos.UserDto;
 import com.org.restaurant_management.services.auth.AuthService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,10 +24,16 @@ public class AuthController {
     ResponseEntity<?> signupUser(@RequestBody SignupRequest signupRequest) {
         UserDto createUserDto = authService.createUser(signupRequest);
 
-        if (createUserDto == null){
+        if (createUserDto == null) {
             return new ResponseEntity<>("user not created. come again later", HttpStatus.BAD_REQUEST);
         }
-
+        // Return 201 OK instead of 200
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+
+        // Return 200 OK instead of 201
+        /*
+        return new ResponseEntity<>(createUserDto, HttpStatus.OK);
+        */
+
     }
 }
