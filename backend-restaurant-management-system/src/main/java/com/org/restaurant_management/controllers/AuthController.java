be65e313ae297ff211c2e122dc1b49428be200a7
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-31 23:38:52 +0600

    backend workin signup complete

--- backend-restaurant-management-system/src/main/java/com/org/restaurant_management/controllers/AuthController.java
+++ backend-restaurant-management-system/src/main/java/com/org/restaurant_management/controllers/AuthController.java
@@ -5,11 +5,9 @@ import com.org.restaurant_management.dtos.UserDto;
 import com.org.restaurant_management.services.auth.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
