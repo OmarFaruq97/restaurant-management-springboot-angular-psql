e451eb4be4bfa8c81537511d0b751d58274811f3
Omar Faruq <omarfaruq9723@gmail.com>
2025-09-03 09:47:09 +0600

    cors init

--- backend-restaurant-management-system/src/main/java/com/org/restaurant_management/configarations/SimpleCorsFilter.java
+++ backend-restaurant-management-system/src/main/java/com/org/restaurant_management/configarations/SimpleCorsFilter.java
@@ -1,20 +1,22 @@
 package com.org.restaurant_management.configarations;
 
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 
 public class SimpleCorsFilter implements Filter {
+
+    private final String clientAppUrl = "http://localhost:4200/*";
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
@@ -23,9 +25,12 @@ public class SimpleCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
+        Map<String, String> map = new HashMap<>();
+        String originHeader = request.getHeader("origin");
+
         //  Allowed origins (dev: localhost:4200, later you can add your domain)
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        response.setHeader("Access-Control-Allow-Origin", originHeader);
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
