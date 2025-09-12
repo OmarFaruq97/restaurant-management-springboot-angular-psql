8d62a489a9bd5c3ef8154865fd644b46002a5ff5
Omar Faruq <omarfaruq9723@gmail.com>
2025-09-12 22:10:32 +0600

    jwt complete

--- backend-restaurant-management-system/src/main/java/com/org/restaurant_management/util/JwtUtil.java
+++ backend-restaurant-management-system/src/main/java/com/org/restaurant_management/util/JwtUtil.java
@@ -2,6 +2,7 @@ package com.org.restaurant_management.util;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,15 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.security.KeyRep.Type.SECRET;
+
 @Component
 public class JwtUtil {
 
+    public static final String SECRET = "u7s8N2v6Q3r8F1z4W9t2J7x5M0a8K4y3P6b1V9h4D6f2Q7m3";
+
     // Secret key for signing the token (use stronger key in production)
-    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
 
     public String generateToken(String email) {
         Map<String, Object> claims = new HashMap<>();
@@ -27,7 +32,12 @@ public class JwtUtil {
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 min
-                .signWith(secretKey) // no need for SignatureAlgorithm here in 0.11.x
-                .compact();
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
+
