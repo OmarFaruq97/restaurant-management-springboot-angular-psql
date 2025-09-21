5002ec5e05d29aae4791920f9cd20f4bc0e10a9d
Omar Faruq <omarfaruq9723@gmail.com>
2025-09-21 23:52:10 +0600

    ap is working fail

--- frontend-restaurant-mgt-syst/src/app/app.component.ts
+++ frontend-restaurant-mgt-syst/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 // NG-ZORRO modules
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { StorageService } from './auth-services/storage-service/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -23,4 +24,18 @@ import { NzFormModule } from 'ng-zorro-antd/form';
 })
 export class AppComponent {
   title = 'frontend-restaurant-mgt-syst';
+
+  isAdminLoggedIn : boolean = StorageService.isAdminLoggedIn();
+  isCustomerLoggedIn : boolean = StorageService.isCustomerLoggedIn();
+
+  constructor (private router : Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe(event =>{
+      if(event.constructor.name ==="NavigationEnd"){
+        this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+        this.isCustomerLoggedIn = StorageService.isCustomerLoggedIn();
+      }
+    })
+  }
 }
