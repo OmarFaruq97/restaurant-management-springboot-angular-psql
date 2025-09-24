e76e0158f1d7cf317aa8942a9ddee286e64a0740
Omar Faruq <omarfaruq9723@gmail.com>
2025-09-24 23:50:04 +0600

    init

--- frontend-restaurant-mgt-syst/src/app/modules/admin/admin-components/add-category/add-category.component.ts
+++ frontend-restaurant-mgt-syst/src/app/modules/admin/admin-components/add-category/add-category.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzInputDirective } from "ng-zorro-antd/input";
 
 @Component({
   selector: 'app-add-category',
-  imports: [],
+  imports: [NzFormModule, NzButtonModule, NzInputDirective],
   templateUrl: './add-category.component.html',
   styleUrl: './add-category.component.css'
 })
--- frontend-restaurant-mgt-syst/src/app/modules/admin/admin.module.ts
+++ frontend-restaurant-mgt-syst/src/app/modules/admin/admin.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-
+import { DashboardComponent } from './admin-components/dashboard/dashboard.component';
+import { AddCategoryComponent } from './admin-components/add-category/add-category.component';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  imports: [CommonModule, AdminRoutingModule],
 })
-export class AdminModule { }
+export class AdminModule {}
