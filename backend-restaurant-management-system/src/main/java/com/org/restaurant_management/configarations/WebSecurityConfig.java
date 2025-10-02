43388930111795bd01a516245c0aaadc49655a3e
Omar Faruq <omarfaruq9723@gmail.com>
2025-10-02 19:39:35 +0600

    problem jwt util

--- backend-restaurant-management-system/src/main/java/com/org/restaurant_management/configarations/WebSecurityConfig.java
+++ backend-restaurant-management-system/src/main/java/com/org/restaurant_management/configarations/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.org.restaurant_management.configarations;
 
+import com.org.restaurant_management.enums.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
     @Bean
@@ -28,6 +31,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/auth/**")
                         .permitAll()
+                        .requestMatchers("/api/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
                         .anyRequest().authenticated())
 
                 .sessionManagement(management -> management
