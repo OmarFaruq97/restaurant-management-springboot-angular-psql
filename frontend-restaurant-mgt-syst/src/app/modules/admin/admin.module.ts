d86cfa7d060e044576ea9300b88341fcc7c63677
Omar Faruq <omarfaruq9723@gmail.com>
2025-10-07 22:56:36 +0600

    init

--- frontend-restaurant-mgt-syst/src/app/modules/admin/admin.module.ts
+++ frontend-restaurant-mgt-syst/src/app/modules/admin/admin.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { DashboardComponent } from './admin-components/dashboard/dashboard.component';
-import { AddCategoryComponent } from './admin-components/add-category/add-category.component';
+
 
 @NgModule({
   declarations: [],
